fde692a070c5cebb8bcf71a790ec86f3102854c8
Wilson Santos <wkadoshi@gmail.com>
2022-08-31 11:18:32 -0300

    ajuste layout - columns

--- src/Components/Seat/index.js
+++ src/Components/Seat/index.js
@@ -19,21 +19,23 @@ export default function Seat({ columns, changeSeat }) {
             {listColumns && listColumns.map((column, columnIndex) =>
                 <div key={columnIndex} className="column">
                     <p><b>{column.columnName}</b></p>
-                    {column.rows.map((row, rowIndex) =>
-                        < div key={rowIndex} className={columnIndex === 0 ? "row row-first" : columnIndex === listColumns.length - 1 ? "row row-last" : "row"}>
-                            {columnIndex === 0 &&
-                                <b><p className='row-number'>{row.rowNumber < 10 ? "0" + row.rowNumber : row.rowNumber}</p></b>
-                            }
-                            <div className='seats-row'>
-                                {row.seats && row.seats.map((seat, seatIndex) =>
-                                    <div key={seatIndex} onClick={() => handleSetSeat(column.columnName + "-" + row.rowNumber + "-" + seat.number)}>
-                                        <EventSeatIcon color={seat.filled ? 'disabled' : 'success'} />
-                                        <p className='seat-name-event'>{seat.number}</p>
-                                    </div>
-                                )}
+                    <div className={columnIndex !== 0 && columnIndex !== listColumns.length - 1 ? "column-margin-top": ""}>
+                        {column.rows.map((row, rowIndex) =>
+                            < div key={rowIndex} className={columnIndex === 0 ? "row row-first" : columnIndex === listColumns.length - 1 ? "row row-last" : "row"}>
+                                {columnIndex === 0 &&
+                                    <b><p className='row-number'>{row.rowNumber < 10 ? "0" + row.rowNumber : row.rowNumber}</p></b>
+                                }
+                                <div className='seats-row'>
+                                    {row.seats && row.seats.map((seat, seatIndex) =>
+                                        <div key={seatIndex} onClick={() => handleSetSeat(column.columnName + "-" + row.rowNumber + "-" + seat.number)}>
+                                            <EventSeatIcon color={seat.filled ? 'disabled' : 'success'} />
+                                            <p className='seat-name-event'>{seat.number}</p>
+                                        </div>
+                                    )}
+                                </div>
                             </div>
-                        </div>
-                    )}
+                        )}
+                    </div>
                 </div>
             )
             }
